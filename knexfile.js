709b4f13395d9dfdeb482099a1319db897f0d5d8
Turner Hayes <dev@turnerhayes.com>
2019-03-25 20:55:26 -0400

    Add DB migration postinstall step, with warning if DB URL not set
    - Also changed `if-env` npm commands for `per-env` commands

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,17 @@ require("dotenv").config({
   path: path.join(__dirname, ".env"),
 });
 
+const dbURL = process.env.DATA_DB_URL;
+
+if (!dbURL) {
+  // eslint-disable-next-line no-console
+  console.error(
+    // eslint-disable-next-line max-len
+    "No database URL set; cannot run migration. Set the DATA_DB_URL environment variable and try again"
+  );
+}
+
 module.exports = {
   client: "pg",
-  connection: process.env.DATA_DB_URL,
+  connection: dbURL,
 };
