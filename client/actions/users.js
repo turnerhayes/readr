820f1415637b8981946cee65cac0eff102bcfc81
Turner Hayes <dev@turnerhayes.com>
2019-05-10 19:03:29 -0400

    Refactor API call actions to use util function to reduce duplication of code

--- client/actions/users.js
+++ client/actions/users.js
@@ -1,51 +1,19 @@
+import {
+  createAPIAction,
+} from "+app/actions/utils";
 import * as api from "+app/api";
-
-export const FETCH_GET_USERS_START = "FETCH_GET_USERS_START";
-
-export const FETCH_GET_USERS_FAIL = "FETCH_GET_USERS_FAIL";
-
-export const FETCH_GET_USERS_COMPLETE = "FETCH_GET_USERS_COMPLETE";
 
 /**
  * Action creator for fetching users
  *
  * @return {function} an action creator function
  */
-export function fetchUsers({ ids } = {}) {
-  return async (dispatch) => {
-    try {
-      dispatch({
-        type: FETCH_GET_USERS_START,
-        payload: {},
-        api: {
-          callName: "fetchUsers",
-          status: "started",
-        },
-      });
+export const fetchUsers = createAPIAction(
+  async function fetchUsers({ ids } = {}) {
+    const users = await api.getUsers({ ids });
 
-      const users = await api.getUsers({ ids });
-
-      dispatch({
-        type: FETCH_GET_USERS_COMPLETE,
-        payload: {
-          users,
-        },
-        api: {
-          callName: "fetchUsers",
-          status: "complete",
-        },
-      });
-
-      return users;
-    } catch (ex) {
-      dispatch({
-        type: FETCH_GET_USERS_FAIL,
-        api: {
-          callName: "fetchUsers",
-          status: "complete",
-        },
-        error: ex,
-      });
-    }
-  };
-}
+    return {
+      users,
+    };
+  }
+);
